78c75ed3dc1e7db5aa2b1dda8abee46e484a0cab
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2019-10-16 12:27:48 -0700

    Random percent and output convert to string

--- Game_of_Life/src/GameOfLife.java
+++ Game_of_Life/src/GameOfLife.java
@@ -1,64 +1,51 @@
 import javax.swing.*;
+import java.io.IOException;
 import java.util.*;
 
 public class GameOfLife {
+    private static int generationTimes = 0;
 
-    public static void main(String[] args) {
-        /***
-         * test
-         * int orangin[][] = {
-         *                 {0, 0, 1, 0, 1, 0},
-         *                 {0, 0, 0, 1, 1, 0},
-         *                 {0, 0, 0, 1, 0, 0},
-         *                 {0, 0, 0, 0, 0, 0},
-         *                 {0, 0, 0, 0, 0, 0},
-         *                 {0, 0, 0, 0, 0, 0},
-         *         };
-         */
+    public static void main(String[] args) throws InterruptedException {
 
-        //Windows test
-        JFrame frame = new JFrame("GameOfLifeWindows");
-        frame.setContentPane(new GameOfLifeWindows().mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-
-
-        Random r = new Random();
         /*input grid width*/
         Scanner input = new Scanner(System.in);
-        System.out.println("Here will create a grid and random origin cell: ");
+        System.out.println("Here will create a grid and random origin cell");
         System.out.print("Please input grid width : ");
         int gridx = input.nextInt();
         System.out.print("Please input grid height : ");
         int gridy = input.nextInt();
+        System.out.print("Please input max generation times (if you want infinite please type 0): ");
+        int maxGenerationTimes = input.nextInt();
 
-        /*for test size*/
-//        int gridx = 20;
-//        int gridy = 20;
+        int[][] origin = CreateRandomCellGrid(gridy, gridx);
 
-        int origin[][] = new int[gridx][gridy];
-        for (int i = 0; i < gridx; i++) {
-            for (int j = 0; j < gridy; j++) {
-                origin[i][j] = r.nextInt((1 - 0) + 1) + 0;
-            }
-        }
-
-        /*for test*/
+        /*for test date*/
 //        int origin[][] = {
 //                {0, 0, 0, 0, 0, 0},
-//                {0, 1, 1, 0, 0, 0},
-//                {0, 1, 0, 0, 0, 0},
-//                {0, 0, 0, 0, 1, 0},
-//                {0, 0, 0, 1, 1, 0},
 //                {0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0},
+//                {0, 1, 0, 0, 1, 0},
+//                {0, 1, 0, 0, 1, 0},
+//                {0, 0, 1, 0, 0, 0},
 //        };
 
 //        input.close();
-
+        System.out.println(generationTimes++);
+        OutputCellPlayGrid(origin);
         while (true) {
+            if (generationTimes == maxGenerationTimes) {
+                System.out.println("Ooops is time to stop!");
+                break;
+            }
+
+            System.out.println(generationTimes);
             origin = NextGeneration(origin);
             System.out.println();
+
+            if (!CheckAllCellAlive(origin)) {
+                System.out.println("Ooops All cells are dead");
+                break;
+            }
         }
     }
 
@@ -71,7 +58,7 @@ public class GameOfLife {
      * @return
      */
 
-    public static int[][] NextGeneration(int[][] cells) {
+    public static int[][] NextGeneration(int[][] cells) throws InterruptedException {
         int[][] next = new int[cells.length][cells[0].length];
         int neighbourAliveCount = 0;
 
@@ -79,63 +66,115 @@ public class GameOfLife {
             for (int j = 0; j < cells[i].length; j++) {
 
                 /*scan it neighbours*/
-                neighbourAliveCount = 0;
-                for (int y = -1; y < 2; y++) {
-                    for (int x = -1; x < 2; x++) {
-                        if (x == 0 && y == 0) {
-                            /*skip it self*/
-                            continue;
-                        }
+                neighbourAliveCount = GetNeighbourAliveCount(cells, i, j);
 
-                        int cellx = j + x;
-                        int celly = i + y;
-
-                        /*for infinite,te loop*/
-                        cellx = cellx < 0 ? cells[i].length - 1 : cellx;
-                        cellx = cellx >= cells[i].length ? 0 : cellx;
-                        celly = celly < 0 ? cells.length - 1 : celly;
-                        celly = celly >= cells.length ? 0 : celly;
-
-                        if (cells[celly][cellx] == 1) {
-                            neighbourAliveCount++;
-                        }
-                    }
-                }
-
-                /*Any live cell with fewer than two live neighbours dies, as if by underpopulation.*/
+                /* Rule1
+                Any live cell with fewer than two live neighbours dies, as if by underpopulation.*/
                 if (neighbourAliveCount < 2) {
                     next[i][j] = 0;
                     continue;
                 }
 
-                /*Any live cell with two or three live neighbours lives on to the next generation.*/
-                if (neighbourAliveCount == 2) {
+                /*Rule2
+                Any live cell with two or three live neighbours lives on to the next generation.*/
+                if (neighbourAliveCount == 2 || neighbourAliveCount == 3) {
                     next[i][j] = cells[i][j];
+
+                    /*Rule4
+                    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.*/
+                    if (neighbourAliveCount == 3) {
+                        next[i][j] = 1;
+                    }
                     continue;
                 }
 
-                /*Any live cell with more than three live neighbours dies, as if by overpopulation.*/
+                /*Rule3
+                Any live cell with more than three live neighbours dies, as if by overpopulation.*/
                 if (neighbourAliveCount > 3) {
                     next[i][j] = 0;
                     continue;
                 }
+            }
+        }
+        generationTimes++;
+        /*output*/
+        OutputCellPlayGrid(next);
 
-                /*Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.*/
-                if (neighbourAliveCount == 3) {
-                    next[i][j] = 1;
+        Thread.sleep(300);
+        return next;
+    }
+
+    private static void OutputCellPlayGrid(int[][] next) {
+        /*for convert to String, for clear to see*/
+        String[][] nextStr = new String[next.length][next[0].length];
+        for (int i = 0; i < next.length; i++) {
+            for (int j = 0; j < next[i].length; j++) {
+                nextStr[i][j] = next[i][j] == 0 ? " " : "■";
+            }
+            System.out.print(Arrays.toString(nextStr[i]));
+            System.out.println();
+        }
+    }
+
+    private static int GetNeighbourAliveCount(int[][] cells, int i, int j) {
+        int neighbourAliveCount = 0;
+        /*scan neighbour in 3*3 */
+        for (int y = -1; y < 2; y++) {
+            for (int x = -1; x < 2; x++) {
+
+                /*skip it self*/
+                if (x == 0 && y == 0) {
                     continue;
+                }
+
+                int cellx = j + x;
+                int celly = i + y;
+
+                /*for infinite loop*/
+                cellx = cellx < 0 ? cells[i].length - 1 : cellx;
+                cellx = cellx >= cells[i].length ? 0 : cellx;
+                celly = celly < 0 ? cells.length - 1 : celly;
+                celly = celly >= cells.length ? 0 : celly;
+
+                /*1 is alive*/
+                if (cells[celly][cellx] == 1) {
+                    neighbourAliveCount++;
                 }
             }
         }
+        return neighbourAliveCount;
+    }
 
-        /*output*/
-        for (int i = 0; i < next.length; i++) {
-            for (int j = 0; j < next[i].length; j++) {
-                System.out.print(next[i][j]);
+    private static int[][] CreateRandomCellGrid(int gridx, int gridy) {
+
+        int origin[][] = new int[gridx][gridy];
+        for (int i = 0; i < gridx; i++) {
+            for (int j = 0; j < gridy; j++) {
+                origin[i][j] = RandomBooleanByPercent(50) ? 1 : 0;
             }
+        }
+        return origin;
+    }
+
+    private static boolean RandomBooleanByPercent(int percent) {
+        Random r = new Random();
+        return ((r.nextInt(100) + 1) <= percent);
+    }
+
+    private static boolean CheckAllCellAlive(int[][] cells) {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                if (cells[i][j] == 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void CleanScreen() {
+        for (int i = 0; i <= 100; i++) {
             System.out.println();
         }
-
-        return next;
     }
 }
--- Game_of_Life/src/GameOfLifeWindows.java
+++ Game_of_Life/src/GameOfLifeWindows.java
@@ -1,9 +1,47 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class GameOfLifeWindows {
-    private JButton button1;
-    private JTextField textField1;
-    private JTextField textField2;
-    public JPanel mainPanel;
+public class GameOfLifeWindows extends JFrame{
+    private JButton createButton;
+    private JTextField gridWidth;
+    private JTextField gridHeight;
+    private JPanel mainPanel;
     private JPanel cellPlayGrid;
+    private JButton stopButton;
+
+    public GameOfLifeWindows() {
+//        cellPlayGrid.add(new JLabel("test"));
+        add(mainPanel);
+        setTitle("GameOfLifeWindows");
+        setSize(1000,800);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+
+          GameOfLifeWindows gameOfLifeWindows = new GameOfLifeWindows();
+          gameOfLifeWindows.setVisible(true);
+
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                GameOfLifeWindows gameOfLifeWindows = new GameOfLifeWindows();
+//                gameOfLifeWindows.setVisible(true);
+//            }
+//        });
+
+    }
 }
