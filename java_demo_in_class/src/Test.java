f27bd75c293a559eabe6d4f3d9e419b2bb74d77f
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2019-11-04 12:25:51 -0800

    Add GUI Password Generator

--- java_demo_in_class/src/Test.java
+++ java_demo_in_class/src/Test.java
@@ -1,11 +1,19 @@
 public class Test {
     public static void main(String[] args) {
-        char t = 8;
-        System.out.println(t);
-        Fish f = new Fish("red");
-        System.out.println(Fish.staticColor);
-        System.out.println(f.color);
-        System.out.println(f.getClass().getSimpleName());
+        int x = 3;
+        System.out.println( x & 3);
+        System.out.println( x | 3);
+        System.out.println( x ^ 3);
+        System.out.println( x << 3);
+        System.out.println( x >> 3);
+
+
+//        char t = 8;
+//        System.out.println(t);
+//        Fish f = new Fish("red");
+//        System.out.println(Fish.staticColor);
+//        System.out.println(f.color);
+//        System.out.println(f.getClass().getSimpleName());
     }
     public class Cat{
         Cat(){
