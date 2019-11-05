21380f993aa7bfdf1755a1e01c2e8fd042e01b01
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2019-11-05 08:43:33 -0800

    Change Something

--- Aaron_GUI_Based_Password_Generator/src/PasswordGeneratorWindow.java
+++ Aaron_GUI_Based_Password_Generator/src/PasswordGeneratorWindow.java
@@ -5,6 +5,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 
 public class PasswordGeneratorWindow extends JFrame {
     private AaronJCheckBox uppercaseCheckBox;
