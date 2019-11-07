ff134dc9046f360f7638ea68a55ad2fb3d5a03eb
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2019-11-06 22:15:04 -0800

    Add Guess Number

--- GuessNumber/src/GuessNumberWindow.java
+++ GuessNumber/src/GuessNumberWindow.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-public class GuessNumberWindow {
+/*
+*Option 2
+Create a GUI Application that will challenge the user to a 'guessing game'
+1) Allow users to generate 3 or more or infinite guesses
+2) Allow user to dictate 'range' of the numbers allowed in the game
+3) Let the user know if he is 'closer' or 'further away', 'higher or lower' than the
+number the computer has
+4) Allow user to choose if he should receive a hint or not based on rule 3
+* */
+
+public class GuessNumberWindow extends JFrame {
+
+    private JSpinner guessingTimesSpinner;
 }
