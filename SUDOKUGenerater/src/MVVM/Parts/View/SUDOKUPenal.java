6f9cb637573d8940bdb91f3ae70835cbdc4b0111
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2019-12-02 10:05:52 -0800

    JTextPane only allow input number

--- SUDOKUGenerater/src/MVVM/Parts/View/SUDOKUPenal.java
+++ SUDOKUGenerater/src/MVVM/Parts/View/SUDOKUPenal.java
@@ -1,5 +1,6 @@
 package MVVM.Parts.View;
 
+import Bootstrap.Tools.JNumberTextPane;
 import MVVM.Parts.Model.SUDOKUGeneraterModel;
 
 import javax.swing.*;
@@ -7,9 +8,11 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class SUDOKUPenal extends JPanel {
 
+    String[][] SUDOKUQuestionTable;
     public SUDOKUPenal() {
         super();
         setLayout(new GridLayout(3, 3));
@@ -17,7 +20,7 @@ public class SUDOKUPenal extends JPanel {
 
     public void generate(int difficulty) {
         removeAll();
-        String[][] SUDOKUQuestionTable = SUDOKUGeneraterModel.generate(difficulty);
+        SUDOKUQuestionTable = SUDOKUGeneraterModel.generate(difficulty);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 JPanel smallPanel = new JPanel(new GridLayout(3, 3));
@@ -26,7 +29,24 @@ public class SUDOKUPenal extends JPanel {
                     for (int sj = 0; sj < 3; sj++) {
                         int ip = i * 3 + si;
                         int jp = j * 3 + sj;
-                        JTextPane n = new JTextPane();
+                        JNumberTextPane n = new JNumberTextPane() {
+                            @Override
+                            public void processKeyEvent(KeyEvent ev) {
+                                if (Character.isDigit(ev.getKeyChar()) || ev.getKeyChar() == 8) {
+                                    super.processKeyEvent(ev);
+                                    String inputString = ev.getKeyChar()+"";
+                                    JNumberTextPane thisPane = ((JNumberTextPane) ev.getSource());
+                                    thisPane.setText(inputString);
+                                    SUDOKUQuestionTable[ip][jp] = inputString;
+
+                                    if(SUDOKUGeneraterModel.allowPutChecker(SUDOKUQuestionTable,ip,jp,inputString)){
+                                        thisPane.setForeground(Color.gray);
+                                    }else{
+                                        thisPane.setForeground(Color.red);
+                                    }
+                                }
+                            }
+                        };
 
                         StyledDocument doc = n.getStyledDocument();
                         SimpleAttributeSet center = new SimpleAttributeSet();
@@ -38,7 +58,7 @@ public class SUDOKUPenal extends JPanel {
                         n.setFont(new Font("Microsoft JhengHei", Font.LAYOUT_NO_LIMIT_CONTEXT, 38));
                         n.setForeground(Color.gray);
                         n.setDisabledTextColor(Color.black);
-                        if(!n.getText().equals("")){
+                        if (!n.getText().equals("")) {
                             n.setEnabled(false);
                         }
                         smallPanel.add(n);
